e33cc3f6be20653fa42012c39d4ff0c8201f1e75
CesarCBQ <137097163+CesarCBQ@users.noreply.github.com>
2025-09-17 05:41:28 +0000

    actualizacion cssyhtml

---
+++ script.js
@@ -0,0 +1,23 @@
+const track = document.querySelector('.carrusel-track');
+const prevBtn = document.querySelector('.carrusel-btn.prev');
+const nextBtn = document.querySelector('.carrusel-btn.next');
+const images = document.querySelectorAll('.carrusel img');
+
+let index = 0;
+
+function updateCarousel() {
+  const width = images[0].clientWidth + 20; 
+  track.style.transform = `translateX(-${index * width}px)`;
+}
+
+nextBtn.addEventListener('click', () => {
+  index = (index + 1) % images.length;
+  updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+  index = (index - 1 + images.length) % images.length;
+  updateCarousel();
+});
+
+window.addEventListener('resize', updateCarousel);
