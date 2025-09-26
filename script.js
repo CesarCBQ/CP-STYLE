f6d07584002d9a9c4db8342f9b6caca511ebd13a
CesarCBQ <137097163+CesarCBQ@users.noreply.github.com>
2025-09-26 07:49:23 +0000

    cambio todo

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-const track = document.querySelector('.carrusel-track');
-const prevBtn = document.querySelector('.carrusel-btn.prev');
-const nextBtn = document.querySelector('.carrusel-btn.next');
-const images = document.querySelectorAll('.carrusel img');
+document.querySelectorAll('.carrusel').forEach(carrusel => {
+  const track = carrusel.querySelector('.carrusel-track');
+  const prevBtn = carrusel.querySelector('.carrusel-btn.prev');
+  const nextBtn = carrusel.querySelector('.carrusel-btn.next');
+  const images = carrusel.querySelectorAll('img');
 
-let index = 0;
+  let index = 0;
 
-function updateCarousel() {
-  const width = images[0].clientWidth + 20; 
-  track.style.transform = `translateX(-${index * width}px)`;
-}
+  function updateCarousel() {
+    const width = images[0].clientWidth + 20;
+    track.style.transform = `translateX(-${index * width}px)`;
+  }
 
-nextBtn.addEventListener('click', () => {
-  index = (index + 1) % images.length;
-  updateCarousel();
+  nextBtn.addEventListener('click', () => {
+    index = (index + 1) % images.length;
+    updateCarousel();
+  });
+
+  prevBtn.addEventListener('click', () => {
+    index = (index - 1 + images.length) % images.length;
+    updateCarousel();
+  });
+
+  window.addEventListener('resize', updateCarousel);
 });
-
-prevBtn.addEventListener('click', () => {
-  index = (index - 1 + images.length) % images.length;
-  updateCarousel();
-});
-nextBtn.addEventListener('click', () => {
-  Cadenas = (Cadenas + 1) % images.length;
-  updateCarousel();
-});
-prevBtn.addEventListener('click', () => {
-  Cadenas = (Cadenas - 1 + images.length) % images.length;
-  updateCarousel();
-});
-
-window.addEventListener('resize', updateCarousel);
